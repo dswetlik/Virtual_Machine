9c8fccb743839336b361de7e7b2a7c3b63c92192
Diedrich Swetlik <diedrich.swetlik@hotmail.com>
2021-10-20 14:29:54 -0500

    Decoding added to machine.py, working

--- machine.py
+++ machine.py
@@ -61,7 +61,7 @@ def run():
     while(''.join(instructionRegister[0:4]) != '0000'
           and programCounter <= maxCounter):
 
-        decode()
+        decode(''.join(instructionRegister))
 
         num += 1
         for x in range(len(bin(num)[2:].zfill(16))):
@@ -75,8 +75,97 @@ def fetch(num):
         instructionRegister[x] = memory[num][x]
 
 
-def decode():
-    print(''.join(instructionRegister))
+def decode(strReg):
+
+    print(strReg)
+    opcode = ""
+    registerA = ""
+    registerB = ""
+    destinationRegister = ""
+
+    if strReg[0:4] == '0000':
+        opcode = "HALT"
+    elif strReg[0:4] == '0001':
+        opcode = "ADD"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        if strReg[10] == '0':
+            registerB = translateregister(strReg[13:16])
+            print(opcode + " " + destinationRegister + " " + registerA + " " + registerB)
+        else:
+            print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[11:16])
+    elif strReg[0:4] == '0010':
+        opcode = "AND"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        if strReg[10] == '0':
+            registerB = translateregister(strReg[13:16])
+            print(opcode + " " + destinationRegister + " " + registerA + " " + registerB)
+        else:
+            print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[11:16])
+    elif strReg[0:4] == '0011':
+        opcode = "NOT"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + destinationRegister + " " + registerA)
+    elif strReg[0:4] == '0100':
+        opcode = "LD"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
+    elif strReg[0:4] == '0101':
+        opcode = "LDI"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
+    elif strReg[0:4] == '0110':
+        opcode = "LDR"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[10:16])
+    elif strReg[0:4] == '0111':
+        opcode = "ST"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
+    elif strReg[0:4] == '1000':
+        opcode = "STI"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
+    elif strReg[0:4] == '1001':
+        opcode = "STR"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[10:16])
+    elif strReg[0:4] == '1010':
+        opcode = "GET"
+        destinationRegister = translateregister(strReg[4:7])
+    elif strReg[0:4] == '1011':
+        opcode = "PUT"
+        destinationRegister = translateregister(strReg[4:7])
+    elif strReg[0:4] == '1100':
+        opcode = "BR"
+        print(opcode + " " + strReg[4:7] + " " + strReg[7:16])
+    elif strReg[0:4] == '1101':
+        if strReg[4] == 0:
+            opcode = "JMP"
+        else:
+            opcode = "JSR"
+        print(opcode + " " + strReg[7:16])
+    elif strReg[0:4] == '1110':
+        if strReg[4] == 0:
+            opcode = "JMPR"
+        else:
+            opcode = "JSRR"
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + registerA + " " + strReg[10:16])
+    elif strReg[0:4] == '1111':
+        opcode = "RET"
+        print(opcode)
+    else:
+        print("Invalid Command Exception: Given Command Is Not Valid")
+
+
+def translateregister(regStr):
+    regVal = "R" + str(int(regStr, base=2))
+    return regVal
 
 
 def load(string):
--- test.py
+++ test.py
@@ -15,34 +15,75 @@ def decode(strReg):
         if strReg[10] == '0':
             registerB = translateregister(strReg[13:16])
             print(opcode + " " + destinationRegister + " " + registerA + " " + registerB)
+        else:
+            print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[11:16])
     elif strReg[0:4] == '0010':
         opcode = "AND"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        if strReg[10] == '0':
+            registerB = translateregister(strReg[13:16])
+            print(opcode + " " + destinationRegister + " " + registerA + " " + registerB)
+        else:
+            print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[11:16])
     elif strReg[0:4] == '0011':
         opcode = "NOT"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + destinationRegister + " " + registerA)
     elif strReg[0:4] == '0100':
         opcode = "LD"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
     elif strReg[0:4] == '0101':
         opcode = "LDI"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
     elif strReg[0:4] == '0110':
         opcode = "LDR"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[10:16])
     elif strReg[0:4] == '0111':
         opcode = "ST"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
     elif strReg[0:4] == '1000':
         opcode = "STI"
+        destinationRegister = translateregister(strReg[4:7])
+        print(opcode + " " + destinationRegister + " " + strReg[7:16])
     elif strReg[0:4] == '1001':
         opcode = "STR"
+        destinationRegister = translateregister(strReg[4:7])
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + destinationRegister + " " + registerA + " " + strReg[10:16])
     elif strReg[0:4] == '1010':
         opcode = "GET"
+        destinationRegister = translateregister(strReg[4:7])
     elif strReg[0:4] == '1011':
         opcode = "PUT"
+        destinationRegister = translateregister(strReg[4:7])
     elif strReg[0:4] == '1100':
         opcode = "BR"
+        print(opcode + " " + strReg[4:7] + " " + strReg[7:16])
     elif strReg[0:4] == '1101':
-        opcode = "JMP"
+        if strReg[4] == 0:
+            opcode = "JMP"
+        else:
+            opcode = "JSR"
+        print(opcode + " " + strReg[7:16])
     elif strReg[0:4] == '1110':
-        opcode = "JMPR"
+        if strReg[4] == 0:
+            opcode = "JMPR"
+        else:
+            opcode = "JSRR"
+        registerA = translateregister(strReg[7:10])
+        print(opcode + " " + registerA + " " + strReg[10:16])
     elif strReg[0:4] == '1111':
         opcode = "RET"
+        print(opcode)
+    else:
+        print("Invalid Command Exception: Given Command Is Not Valid")
 
 
 def translateregister(regStr):
@@ -50,6 +91,12 @@ def translateregister(regStr):
     return regVal
 
 
-instructionRegister = [0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1]
-strI = "".join([str(x) for x in instructionRegister])
-decode(strI)
+instructionRegisterA = [0,0,1,1,0,0,0,0,0,0,1,1,1,1,1,1]
+instructionRegisterB = [0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1]
+instructionRegisterC = [0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,0]
+strI1 = "".join([str(x) for x in instructionRegisterA])
+strI2 = "".join([str(x) for x in instructionRegisterB])
+strI3 = "".join([str(x) for x in instructionRegisterC])
+decode(strI1)
+decode(strI2)
+decode(strI3)
